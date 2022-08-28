59c44fea2ac1b3db7cb9e99a0d8e397da0ad0f79
Glitch (glitch-hello-website) <none>
2022-08-28 02:20:01 +0000

    👚⛈ Checkpoint ./script.js:16084306/1554

--- script.js
+++ script.js
@@ -8,3 +8,37 @@ const PI_DIGITS = [
 document.getElementById("pi").textContent = PI_DIGITS;
 
 
+// create a header
+;
+(function() {
+  const navLinks = {
+    "Home": "/index.html",
+    "Mathematics": "/maths.html"
+  };
+  
+  const nav = document.createElement("nav");
+  nav.className = "navbar navbar-expand-lg bg-light";
+  nav.innerHTML = `
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="navbar-list">
+        <li class="nav-item">
+        </li>
+      </ul>
+    </div>
+  </div>`;
+  
+  const linkList = nav.getElementById("navbar-list");
+  for (let pageName in navLinks) {
+    const li = document.createElement("li");
+    li.innerHTML = `<a class="nav-link" aria-current="page" href="${navLinks[pageName]}">${pageName}</a>`;
+    navLinks.appendChild(li);
+  }
+  
+  const mainContainer = document.querySelector("body > .container");
+  mainContainer.prependChild(nav);
+})();
